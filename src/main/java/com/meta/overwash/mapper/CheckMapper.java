9e399530e5cba576f171e3db27d7721ee76eec09
sojungleee <sojong1003@gmail.com>
2023-03-03 09:54:36 +0900

    FEAT: Create CheckMapper

---
+++ src/main/java/com/meta/overwash/mapper/CheckMapper.java
@@ -0,0 +1,17 @@
+package com.meta.overwash.mapper;
+
+import java.util.List;
+
+import com.meta.overwash.domain.Check;
+
+public interface CheckMapper {
+	public List<Check> getList();
+
+	public Integer get();
+
+	public void checkInsert(Check check);
+
+	public int checkUpdate(Check check);
+	
+	public int checkDelete(Check checkId); 
+}
