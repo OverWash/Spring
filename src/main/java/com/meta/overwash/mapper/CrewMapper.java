62e0adceb7c108d3161b891042caf83dcc7b9e78
sojungleee <sojong1003@gmail.com>
2023-03-03 09:54:26 +0900

    FEAT: Create CrewMapper

---
+++ src/main/java/com/meta/overwash/mapper/CrewMapper.java
@@ -0,0 +1,17 @@
+package com.meta.overwash.mapper;
+
+import java.util.List;
+
+import com.meta.overwash.domain.Crew;
+
+public interface CrewMapper {
+	public List<Crew> getList();
+
+	public Integer get();
+
+	public void crewInsert(Crew crew);
+
+	public int crewUpdate(Crew crew);
+	
+	public int crewDelete(Crew crewId); 
+}
