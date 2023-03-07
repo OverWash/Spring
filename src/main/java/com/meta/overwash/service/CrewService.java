74da1106e7ae582e58c00a42b366e3851e6422ea
ysb4642 <ysb4642@naver.com>
2023-03-07 18:00:35 +0900

    crew 기능 개발중

---
+++ src/main/java/com/meta/overwash/service/CrewService.java
@@ -0,0 +1,8 @@
+package com.meta.overwash.service;
+
+import com.meta.overwash.domain.CrewDTO;
+
+public interface CrewService {
+	
+	public int insertCrew(CrewDTO crewDTO);
+}
---
+++ src/main/java/com/meta/overwash/service/CrewServiceImpl.java
@@ -0,0 +1,16 @@
+package com.meta.overwash.service;
+
+import org.springframework.stereotype.Service;
+
+import com.meta.overwash.domain.CrewDTO;
+
+@Service
+public class CrewServiceImpl implements CrewService {
+
+	@Override
+	public int insertCrew(CrewDTO crewDTO) {
+		
+		return 0;
+	}
+	
+}
