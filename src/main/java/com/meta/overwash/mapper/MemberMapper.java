1a54a9305d0f677c567e337166a95880d89c1b33
sojung lee <sojong1003@gmail.com>
2023-03-10 10:12:11 +0900

    FEAT : Member Update

--- src/main/java/com/meta/overwash/mapper/MemberMapper.java
+++ src/main/java/com/meta/overwash/mapper/MemberMapper.java
@@ -5,18 +5,15 @@ import java.util.List;
 import com.meta.overwash.domain.MemberDTO;
 
 public interface MemberMapper {
-
-	public List<MemberDTO> getList();
-
-	public void insertMember(MemberDTO member);
 	
-	public void insertSelectKeyMember(MemberDTO member);
-
-	public MemberDTO getMember(Long memberId);
+	public void insertMember(MemberDTO member) throws Exception;
 	
-	public int updateMember(MemberDTO member);
-
-	public int deleteMember(Long memberId);
+	public List<MemberDTO> getMemberList(String role) throws Exception;
 	
-	public MemberDTO loginMember(MemberDTO member);
+	public MemberDTO getMember(Long memberId) throws Exception;
+
+	public int updateMember(MemberDTO member) throws Exception;
+	
+	public int deleteMember(Long memberId) throws Exception; 
+	
 }
