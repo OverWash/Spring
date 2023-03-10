534b48fd00ffb863a8cf727901afefe849a04f74
백성준 <qortjdwns120712@gmail.com>
2023-03-10 22:10:13 +0900

    FEAT :고객/ 관리자 결제요청 컨트롤러 및 고객 상세 결제 요청 컨트롤러 추가

--- src/main/java/com/meta/overwash/controller/PaymentRequestController.java
+++ src/main/java/com/meta/overwash/controller/PaymentRequestController.java
@@ -1,15 +1,35 @@
 package com.meta.overwash.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.meta.overwash.domain.PaymentRequestDTO;
+import com.meta.overwash.domain.ReceiptDTO;
+import com.meta.overwash.service.PaymentService;
+
 @Controller
-@RequestMapping("/aa")
+@RequestMapping("/payment")
 public class PaymentRequestController {
 
-	@GetMapping("/bb")
-	public void test() {
+	@Autowired
+	PaymentService paymentService;
 
+	//고객 결제요청 상세페이지
+	@GetMapping("/process/{pno}")
+	public void process(@PathVariable("pno") Long pno, Model model) {
+		model.addAttribute("payment", paymentService.get(pno));
 	}
+	
+	//관리자 결제요청서 발급
+	@PostMapping("/process/detail")
+	public void process(ReceiptDTO receipt) {
+		paymentService.paymentProcess(receipt);
+	}
+	
+	
 }
