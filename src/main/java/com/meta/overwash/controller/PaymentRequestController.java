d89cc5abaea447fe67a0aed347b121ea649dfc43
SJ <100SeongJun>
2023-03-12 23:16:40 +0900

    REFACTOR :결제요청 AdminController -> PaymentController 이동 , 고객 결제요청 상세 페이지이동

--- src/main/java/com/meta/overwash/controller/PaymentRequestController.java
+++ src/main/java/com/meta/overwash/controller/PaymentRequestController.java
@@ -1,35 +1,41 @@
 package com.meta.overwash.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.meta.overwash.domain.PaymentRequestDTO;
-import com.meta.overwash.domain.ReceiptDTO;
+import com.meta.overwash.domain.LaundryDTO;
+import com.meta.overwash.domain.ReservationConfirmedDTO;
 import com.meta.overwash.service.PaymentService;
 
 @Controller
-@RequestMapping("/payment")
 public class PaymentRequestController {
 
 	@Autowired
 	PaymentService paymentService;
 
-	//고객 결제요청 상세페이지
-	@GetMapping("/process/{pno}")
-	public void process(@PathVariable("pno") Long pno, Model model) {
+	// 고객의 상세페이지
+	@GetMapping("/member/process/{pno}")
+	public String process(@PathVariable("pno") Long pno, Model model) {
 		model.addAttribute("payment", paymentService.get(pno));
+		return "/payment/process";
 	}
-	
-	//관리자 결제요청서 발급
-	@PostMapping("/process/detail")
-	public void process(ReceiptDTO receipt) {
-		paymentService.paymentProcess(receipt);
+
+	// 결제 요청
+	// 수정중 (03.12) 세탁물 안받아와짐
+	@PostMapping("/admin/payment/request")
+	public String adminPaymentRequest(ArrayList<LaundryDTO> laundryList, ReservationConfirmedDTO rcDto, Model model) {
+		if (paymentService.requestToAdmin(rcDto.getConfirmId(), laundryList) != null) {
+			model.addAttribute("msg", "결제요청이 완료되었습니다.");
+
+		} else {
+			model.addAttribute("msg", "결제 요청이 취소되었습니다.");
+		}
+		return "/admin/main";
 	}
-	
-	
 }
